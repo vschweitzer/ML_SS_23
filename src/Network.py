46933aed8689efffe9d585be1def629787bbd60f
vschweitzer <valentin.schweitzer98@gmail.com>
2023-05-24 12:34:05 +0200

    Add sklearn methods

--- src/Network.py
+++ src/Network.py
@@ -1,14 +1,20 @@
 import numpy as np
+import pandas as pd
 from FCLayer import FcLayer
 from ACLayer import AcLayer
 from activation_functions import sigmoid, relu, sigmoid_prime
 
 
+
 class Network:
-    def __init__(self, learning_rate):
+    def __init__(self, learning_rate, epochs: int = 1000, expand_dims: bool = True):
         self.layers = []
         self.learning_rate = learning_rate
         self.output = []
+        self.epochs = epochs
+        self.expand_dims = expand_dims
+        self.threshold = 0.5
+        self.last_result = None
 
     # rework when working with the actual datasets
     def preprocess(self, data):
@@ -17,7 +23,7 @@ class Network:
     def add(self, layer):
         self.layers.append(layer)
 
-    def predict(self, test_data):
+    def predict_score(self, test_data):
         # sample dimension first
         samples = len(test_data)
         result = []
@@ -30,12 +36,22 @@ class Network:
                 output = layer.forward_propagation(output)
             result.append(output)
         print("Predict function works")
+        self.last_result = result
         print(result)
         return result
 
-    def train(self, x_train, y_train, epochs):
+    def predict(self, data):
+        scores = self.predict_score(data)
+        return [1 if elem[0] >= self.threshold else 0 for elem in scores]
+
+    def predict_proba(self, data):
+        scores = self.predict_score(data)
+        # Create a probability for each score. The closer the prediction is to the threshold, the lower the probability.
+        return [(elem[0] - self.threshold) / (1.0 - self.threshold) if elem[0] >= self.threshold else elem[0] / self.threshold for elem in scores]
+
+    def train(self, x_train, y_train):
         # Epoch times needed to achieve accurate NN
-        for i in range(epochs):
+        for i in range(self.epochs):
             error = 0
             # result = []
             for j in range(len(x_train)):
@@ -51,3 +67,22 @@ class Network:
                 for layer in reversed(self.layers):
                     error = layer.backward_propagation(error, self.learning_rate)
                     # print(f"error:{error}")
+
+    ##########################################
+    # Methods for compatibility with sklearn #
+    ##########################################
+
+    def fit(self, data, targets):
+        if type(data) == pd.DataFrame or type(data) == pd.Series:
+            data = data.to_numpy()
+ 
+        if type(targets) == pd.DataFrame or type(targets) == pd.Series:
+            targets = targets.to_numpy()
+        
+        if self.expand_dims:
+            data = np.expand_dims(data, axis=1)
+            targets = np.expand_dims(targets, axis=1)
+
+        self.train(data, targets)
+
+        
